fbb77405f1c948e154fcbd08acbe6108fb424114
gingleton <georgeingleton@gmail.com>
2017-11-16 19:35:19 -0500

    React States Lab

---
+++ src/components/comments.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import OneComment from './one-comment';
+
+class Comments extends React.Component {
+constructor(){
+  super();
+  this.state={
+    newComments: ['working?'],
+    input: ''
+  }
+  this.handleInput = this.handleInput.bind(this)
+  this.handleClick = this.handleClick.bind(this)
+}
+
+handleClick(e){
+  e.preventDefault();
+  console.log('I just clicked this thing')
+  this.setState({
+    newComments: this.state.newComments.concat([this.state.input]),
+    input: ''
+  })
+}
+
+handleInput(e){
+  this.setState({
+    input: e.target.value
+  })
+
+  console.log('handleInput update -' + this.state.input)
+}
+
+conponentDidUpdate(){
+  if(this.state.input !== ''){
+    console.log('componentDidUpdate log -' + this.state.input)
+  }
+}
+
+  render() {
+    //debugger
+    const comments = this.props.comments.map( (item, index) => <OneComment key={index} comment={item}/>)
+    const newComments = this.state.newComments.map( (item, index) => <OneComment key={index} newComment={item}/>)
+
+    return (
+      <div>
+
+        <form>
+          <input
+            placeholder="enter comment"
+            onChange={this.handleInput}
+            value={this.state.input}
+          />
+          <button onClick={ this.handleClick}>
+            Add comment
+          </button>
+        </form>
+        <div className="wrap">{newComments}</div>
+        <div className="wrap">{comments}</div>
+      </div>
+
+    );
+  }
+}
+
+export default Comments;
